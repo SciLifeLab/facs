e6da2f70e315b4d629814766cef713a870818f16
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-02-28 17:55:41 +0100

    Added chapmanb local version of galaxy rsync genomes download script

---
+++ tests/utils/galaxy.py
@@ -0,0 +1,153 @@
+"""Retrieve indexed genomes using Galaxy's rsync server resources.
+
+http://wiki.galaxyproject.org/Admin/Data%20Integration
+"""
+from xml.etree import ElementTree
+import glob
+import os
+import subprocess
+from contextlib import contextmanager
+
+# Galaxy community rsync server
+server = "rsync://datacache.g2.bx.psu.edu"
+
+# ## Compatibility definitions
+index_map = {"bowtie": "bowtie_index",
+             "bowtie2": "bowtie2_index",
+             "bwa": "bwa_index",
+             "novoalign": None,
+             "ucsc": "seq",
+             "seq": "sam_index"}
+
+org_remap = {"phix": "phiX",
+             "GRCh37": "hg_g1k_v37",
+             "araTha_tair9": "Arabidopsis_thaliana_TAIR9",
+             "araTha_tair10": "Arabidopsis_thaliana_TAIR10",
+             "WS210": "ce10",
+             "WS220": "ce10"}
+
+# ## Galaxy location files
+
+class LocCols(object):
+    # Hold all possible .loc file column fields making sure the local
+    # variable names match column names in Galaxy's tool_data_table_conf.xml
+    def __init__(self, config, dbkey, file_path):
+        self.dbkey = dbkey
+        self.path = file_path
+        self.value = config.get("value", dbkey)
+        self.name = config.get("name", dbkey)
+        self.species = config.get('species', '')
+        self.index = config.get('index', 'index')
+        self.formats = config.get('index', 'fastqsanger')
+        self.dbkey1 = config.get('index', dbkey)
+        self.dbkey2 = config.get('index', dbkey)
+
+def _get_tool_conf(tool_name):
+    """
+    Parse the tool_data_table_conf.xml from installed_files subfolder and extract
+    values for the 'columns' tag and 'path' parameter for the 'file' tag, returning
+    those as a dict.
+    """
+    tool_conf = {}
+    tdtc = ElementTree.parse(env.tool_data_table_conf_file)
+    tables = tdtc.getiterator('table')
+    for t in tables:
+        if tool_name in t.attrib.get('name', ''):
+            tool_conf['columns'] = t.find('columns').text.replace(' ', '').split(',')
+            tool_conf['file'] = t.find('file').attrib.get('path', '')
+    return tool_conf
+
+
+# ## Finalize downloads
+
+def _finalize_index_seq(fname):
+    """Convert UCSC 2bit file into fasta file.
+    """
+    out_fasta = fname + ".fa"
+    if not os.path.exists(out_fasta):
+        subprocess.check_call(["twoBitToFa", "{base}.2bit {out}".format(
+            base=fname, out=out_fasta)])
+
+finalize_fns = {"ucsc": _finalize_index_seq}
+
+def _finalize_index(idx, fname):
+    """Perform final processing on an rsync'ed index file if necessary.
+    """
+    finalize_fn = finalize_fns.get(idx)
+    if finalize_fn:
+        finalize_fn(fname)
+
+# ## Retrieve data from Galaxy
+
+def rsync_genomes(genome_dir, genomes, genome_indexes):
+    """Top level entry point to retrieve rsync'ed indexes from Galaxy.
+    """
+    for gid in (x[1] for x in genomes):
+        galaxy_gid = org_remap.get(gid, gid)
+        indexes = _get_galaxy_genomes(galaxy_gid, genome_dir, genomes, genome_indexes)
+        _finalize_index("ucsc", indexes["ucsc"])
+        for idx, fname in indexes.iteritems():
+            _finalize_index(idx, fname)
+        prep_locs(galaxy_gid, indexes, {})
+
+def _get_galaxy_genomes(gid, genome_dir, genomes, genome_indexes):
+    """Retrieve the provided genomes and indexes from Galaxy rsync.
+    """
+    out = {}
+    org_dir = os.path.join(genome_dir, gid)
+    if not os.path.exists(org_dir):
+        subprocess.check_call(['mkdir', '-p', 'org_dir'])
+    for idx in genome_indexes:
+        galaxy_index_name = index_map.get(idx)
+        index_file = None
+        if galaxy_index_name:
+            index_file = _rsync_genome_index(gid, galaxy_index_name, org_dir)
+        if index_file:
+            out[idx] = index_file
+        else:
+            print "Galaxy does not support {0} for {1}".format(idx, gid)
+    return out
+
+def _rsync_genome_index(gid, idx, org_dir):
+    """Retrieve index for a genome from rsync server, returning path to files.
+    """
+    org_rsync = "{server}/indexes/{gid}/{idx}/".format(
+        server=server, gid=gid, idx=idx)
+    idx_dir = os.path.join(org_dir, idx)
+    if not os.path.exists(idx_dir):
+        check_dir = subprocess.check_output(["rsync", "--list-only", "{server}".format(server=org_rsync)])
+        if org_dir in check_dir:
+            if not os.path.exists(idx_dir):
+                subprocess.check_call(['mkdir', '-p', idx_dir])
+                print idx_dir
+            with cd(idx_dir):
+                subprocess.check_call(["rsync", "-avzP", org_rsync])
+    if os.path.exists(idx_dir):
+        idxes = glob.glob("{idx_dir}/{gid}.fa*".format(idx_dir=idx_dir, gid=gid))
+
+        if idxes:
+            for i in idxes:
+                if i.endswith('.fa'):
+                    ext = ".fa"
+                else:
+                    ext = ""
+        else:
+            ext = ""
+
+#        ext = ".fa" if (has_fa_ext and idx not in ["seq"]) else ""
+        return os.path.join(idx_dir, gid + ext)
+
+@contextmanager
+def cd(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(old_dir)
+
+def main():
+    _get_galaxy_genomes("hg19", os.path.dirname(__file__), "hg19", ["seq"])
+
+if __name__ == "__main__":
+    main()
