69e2310dbdcb049d93da558ff737dc9f49fe7687
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-10-25 23:36:14 +0200

    Add deconseq db target

--- facs/utils/config.py
+++ facs/utils/config.py
@@ -8,12 +8,13 @@ config = ConfigParser.SafeConfigParser()
 conf_file = config.read([os.path.expanduser('~/.facsrc'), '.facsrc',
             'facs.conf', 'facs.cfg', '/etc/facs.conf'])
 try:
-    # First config file found wins
+# First config file found wins
     config.readfp(open(conf_file[0]))
 
     SERVER = config.get('facs', 'SERVER').rstrip()
     FACS_DB = config.get('facs', 'FACS_DB').rstrip()
     FASTQ_SCREEN_DB = config.get('facs', 'FASTQ_SCREEN_DB').rstrip()
+    DECONSEQ_DB = config.get('facs', 'DECONSEQ_DB').rstrip()
     USERNAME = config.get('facs', 'USERNAME').rstrip()
     PASSWORD = config.get('facs', 'PASSWORD').rstrip()
 except:
