b1f2c2c5407d9ce0927e0301a8e558d9c752cded
Roman Valls Guimera <roman.valls.guimera@scilifelab.se>
2013-10-26 21:20:26 +0200

    Display only datasets with format simngs_org_reads.fastq

--- tests/couchdb/contam_view.js
+++ tests/couchdb/contam_view.js
@@ -9,9 +9,11 @@ function(doc) {
                // Javascript Date.parse() does not support subsecond error format
                var timestamp = doc.timestamp.replace(/\+\d{4}/, '');
                var d1 = Date.parse(timestamp);
+               if(sample_file.match(/simngs_\w+_\d+\.fastq/g)){
+                            emit([bloom_file, sample_file], doc._id);
+               }
 
                //var diff = end_run - begin_run;
-
                emit([sample_file, bloom_file], [d1, doc.contamination_rate]);
            }
        }
